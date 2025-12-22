06ec9cda6c3e1a1f08eb9c7ffb2956dadf921051
jentsechen <jentsechen1009@gmail.com>
2025-12-22 22:25:53 +0800

    Add gen. golden for cpp imp.

---
+++ gen_golden_for_cpp_imp.m
@@ -0,0 +1,54 @@
+sig_par = SigPar('wavelength_m', 0.4, 'pulse_width_sec', 10e-6, 'pulse_rep_freq_hz', 1e3, ...
+                 'bandwidth_hz', 50e6, 'sampling_freq_hz', 64e6);
+imaging_par = ImagingPar('sig_par', sig_par, 'closest_slant_range_m', 4e3);
+writematrix(imaging_par.range_time_axis_sec, 'range_time_axis_sec.csv');
+writematrix(imaging_par.range_freq_axis_hz, 'range_freq_axis_hz.csv');
+writematrix(imaging_par.azimuth_time_axis_sec, 'azimuth_time_axis_sec.csv');
+writematrix(imaging_par.azimuth_freq_axis_hz, 'azimuth_freq_axis_hz.csv');
+
+% load("point_target_echo_signal.mat");
+% save('echo_signal_golden.mat', 'point_target_echo_signal');
+chirp_scaling_algo = ChirpScalingAlgo("imaging_par", imaging_par);
+
+disp("migr_par");
+migr_par = chirp_scaling_algo.migr_par;
+save("./golden/migr_par.mat", "migr_par");
+
+disp("modified_range_fm_rate_hz_s");
+modified_range_fm_rate_hz_s = chirp_scaling_algo.modified_range_fm_rate_hz_s;
+save("./golden/modified_range_fm_rate_hz_s.mat", "modified_range_fm_rate_hz_s");
+
+disp("chirp_scaling");
+chirp_scaling = chirp_scaling_algo.chirp_scaling;
+save("./golden/chirp_scaling.mat", "chirp_scaling");
+
+disp("range_comp_filt");
+range_comp_filt = chirp_scaling_algo.range_comp_filt;
+save("./golden/range_comp_filt.mat", "range_comp_filt");
+
+disp("azimuth_comp_filt");
+azimuth_comp_filt = chirp_scaling_algo.azimuth_comp_filt;
+save("./golden/azimuth_comp_filt.mat", "azimuth_comp_filt");
+
+disp("second_comp_filt");
+second_comp_filt = chirp_scaling_algo.second_comp_filt;
+save("./golden/second_comp_filt.mat", "second_comp_filt");
+
+disp("third_comp_filt");
+third_comp_filt = chirp_scaling_algo.third_comp_filt;
+save("./golden/third_comp_filt.mat", "third_comp_filt");
+
+azimuth_fft_out = chirp_scaling_algo.apply_azimuth_fft(point_target_echo_signal);
+chirp_scaling_out = chirp_scaling_algo.apply_chirp_scaling(azimuth_fft_out);
+range_fft_out = chirp_scaling_algo.apply_range_fft(chirp_scaling_out);
+second_phase_func_out = chirp_scaling_algo.apply_second_phase_func(range_fft_out);
+range_ifft_out = chirp_scaling_algo.apply_range_ifft(second_phase_func_out);
+third_phase_func_out = chirp_scaling_algo.apply_third_phase_func(range_ifft_out);
+csa_out = chirp_scaling_algo.apply_azimuth_ifft(third_phase_func_out);
+save("./golden/azimuth_fft_out.mat", "azimuth_fft_out");
+save("./golden/chirp_scaling_out.mat", "chirp_scaling_out");
+save("./golden/range_fft_out.mat", "range_fft_out");
+save("./golden/second_phase_func_out.mat", "second_phase_func_out");
+save("./golden/range_ifft_out.mat", "range_ifft_out");
+save("./golden/third_phase_func_out.mat", "third_phase_func_out");
+save("./golden/csa_out.mat", "csa_out");
